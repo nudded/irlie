379419d9cc50d0c3d8b0de4a6724000926031d98
Toon Willems <willemstoon@gmail.com>
2011-01-07 19:03:53 +0100

    add some really basic testcases

--- irc_parse.erl
+++ irc_parse.erl
@@ -2,16 +2,26 @@
 
 -export([parse/1]).
 
+-include_lib("eunit/include/eunit.hrl").
+
 % parse the input string
 % returns {ok, Command, [Args]}
 % or      {ok, Prefix, Command, [Args]}
 parse(String) ->
-  [PossiblePrefix | [Command | Args]] = re:split(String, " ", [{return, list}]),
+  [PossiblePrefix | Args] = re:split(String, " ", [{return, list}]),
   case is_prefix(PossiblePrefix) of
-    true -> {ok, PossiblePrefix, Command,
-            parse_args(Args)};
-    false -> {ok, PossiblePrefix, parse_args([Command | Args])}
+    true  -> parse_with_prefix(PossiblePrefix, Args);
+    % in this case PossiblePrefix is the Command
+    false -> parse_without_prefix(PossiblePrefix, Args)
   end.
+
+parse_with_prefix(_, []) -> {error, no_command};
+parse_with_prefix(Prefix, Array) ->
+  [Command | Args] = Array,
+  {ok, Prefix, Command, parse_args(Args)}.
+  
+parse_without_prefix(Command, Array) ->
+  {ok, Command, parse_args(Array)}.
 
 % test if the given string is valid as a irc prefix
 % TODO regex could be better
@@ -24,5 +34,26 @@ is_prefix(Prefix) ->
 % parse the arguments so that "channel,channel test" becomes [["channel",
 % "channel"], "test"]. If you know what i mean.
 parse_args(Args) -> 
-  Splitted = lists:map(fun(Arg) -> re:split(Arg,",",[{return, list}]) end, Args),
-  Splitted.
+  lists:map(fun(Arg) -> parse_arg(Arg) end, Args).
+
+% parse individual argument
+parse_arg(Arg) ->
+  Array = re:split(Arg,",",[{return, list}]),
+  if 
+    length(Array) > 1 -> Array;
+    true              -> lists:nth(1,Array)
+  end.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                             %
+%                             TEST FUNCTIONS                                  %
+%                                                                             %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+parse_test() ->
+  ?assertEqual({ok, "QUIT", []}, parse("QUIT")),
+  ?assertEqual({ok, ":nudded", "QUIT", []}, parse(":nudded QUIT")),
+  ?assertEqual({ok, "NICK", ["nudded"]}, parse("NICK nudded")).
+
+
